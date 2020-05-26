3a509c41ae93b6e5d5400883f6b44fb6720ae50f
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-05-26 17:45:26 +0900

    다리를 지나는 트럭 수정

--- level 2/다리를 지나는 트럭.py
+++ level 2/다리를 지나는 트럭.py
@@ -1,36 +1,62 @@
-def solution(bridge_length, weight, truck_weights):
-    current_time = 0
-    current_weight = 0
+from collections import deque
 
-    truck_in_bridge = list()
 
-    for truck in truck_weights:
-        if current_weight == 0:
-            truck_in_bridge.append((truck, bridge_length))
-            current_weight += truck
+# queue 에 있는 트럭을 distance 만큼 이동시킨다. (pop -> 변환 -> push)
+# distance 만큼 이동시켰을때 다리를 벗어나는 트럭에 대한 처리 필요 (bridge_weight 감소)
+def move_trucks(queue, distance, bridge_weight):
+    length = len(queue)
+    for _ in range(length):
+        w, d = queue.popleft()
+        # distance 만큼 이동시켰을때 다리를 벗어나지 않는 트럭에 대해서만 push 수행
+        if d - distance > 0:
+            queue.append((w, d - distance))
         else:
-            top = truck_in_bridge[0]
-            if (truck + current_weight) <= weight:
-                current_time += 1
-                for idx, item in enumerate(truck_in_bridge):
-                    truck_in_bridge[idx] = (item[0], item[1] - 1)
+            bridge_weight -= w
 
-                if truck_in_bridge[0][1] == 0:
-                    current_weight -= top[0]
-                    truck_in_bridge.pop(0)
-            else:
-                while truck_in_bridge and ((truck + current_weight) > weight):
-                    top = truck_in_bridge.pop(0)
-                    current_time += top[1]
-                    for idx, item in enumerate(truck_in_bridge):
-                        truck_in_bridge[idx] = (item[0], item[1] - top[1])
-                    current_weight -= top[0]
-
-            truck_in_bridge.append((truck, bridge_length))
-            current_weight += truck
-    return current_time + bridge_length + 1
+    return bridge_weight
 
 
-assert solution(2, 10, [7, 4, 5, 6]) == 8
-assert solution(100, 100, [10]) == 101
-assert solution(5, 12, [5, 4, 3, 2, 3, 2, 2]) == 14
+def solution(bridge_length, weight, truck_weights):
+    queue = deque([])   # 다리 역할은 큐로 구현한다
+    time = 0            # 경과 시간
+    bridge_weight = 0   # 다리에 있는 트럭의 무게 합
+
+    for truck_weight in truck_weights:
+        # 현재 트럭이 다리에 진입할 수 있는 경우
+        if bridge_weight + truck_weight <= weight:
+            # 트럭 이동과 그에 따른 다리에 있는 트럭 무게 변화 && 시간 경과
+            bridge_weight = move_trucks(queue, 1, bridge_weight)
+            time += 1
+
+            # 트럭 다리 진입과 그에 따른 다리에 있는 트럭 무게 변화
+            queue.append((truck_weight, bridge_length))
+            bridge_weight += truck_weight
+
+        # 현재 트럭이 다리에 진입할 수 없는 경우
+        else:
+            # 현재 트럭이 다리에 진입할 수 있을때까지 반복
+            while queue:
+                # 이동거리가 가장 짧은 트럭 다리 벗어나고 그에 따른 다리에 있는 트럭 무게 변화 && 시간 경과
+                w, d = queue.popleft()
+                bridge_weight -= w
+                bridge_weight = move_trucks(queue, d, bridge_weight)
+                time += d
+
+                if bridge_weight + truck_weight <= weight:
+                    break
+
+            # 트럭 다리 진입과 그에 따른 다리에 있는 트럭 무게 변화
+            queue.append((truck_weight, bridge_length))
+            bridge_weight += truck_weight
+
+    # 다리에 가장 마지막에 위치한 트럭이 다리를 빠져나오는 시간만큼 시간 추가
+    last_w, last_d = queue.pop()
+    time += last_d
+
+    return time
+
+
+if __name__ == '__main__':
+    assert solution(2, 10, [7, 4, 5, 6]) == 8
+    assert solution(100, 100, [10]) == 101
+    assert solution(5, 12, [5, 4, 3, 2, 3, 2, 2]) == 14
