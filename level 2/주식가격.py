feb04736d32fc50e62cd89ba0948b7886da63795
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-05-30 16:30:08 +0900

    주식가격 수정

--- level 2/주식가격.py
+++ level 2/주식가격.py
@@ -1,16 +1,31 @@
 def solution(prices):
-    times = list()
-    st = list()
-    for price in prices[::-1]:
-        time = 1 if st else 0
+    stack = [(prices[-1], 0)]
+    times = [0]
 
-        while st and st[-1][0] >= price:
-            top = st.pop(-1)
-            time += top[1]
+    # prices 를 역순으로 순회하기 위한 인덱스
+    idx = len(prices) - 2
+    time = 1
 
-        times.append(time)
-        st.append((price, time))
+    while idx >= 0:
+        price = prices[idx]
+        # 주가 유지 시간이 결정된 경우
+        if (not stack) or (stack and price > stack[-1][0]):
+            # 정보 저장
+            stack.append((price, time))
+            times.append(time)
+
+            # 다음 주식 정보를 가리키고 보유 시간을 1로 초기화
+            idx -= 1
+            time = 1
+        # 현재 시점을 기준으로 다음 시점까지의 주가가 떨어지지 않았으므로 주가 유지 시간을 더한다
+        else:
+            _, t = stack.pop(-1)
+            time += t
+
+    # 유지 시간의 역순을 출력
     return times[::-1]
 
 
-assert solution([1, 2, 3, 2, 3]) == [4, 3, 1, 1, 0]
+if __name__ == '__main__':
+    assert solution([1, 2, 3, 2, 3]) == [4, 3, 1, 1, 0]
+    assert solution([1, 4, 4, 3, 4]) == [4, 2, 1, 1, 0]
